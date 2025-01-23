3d948086d350fdc9a2f5759a21b85b4f62c27620
Moulberry <james.jenour@protonmail.com>
2025-01-23 15:53:38 +0800

    Force players to be mountable inside replay

--- src/main/java/com/moulberry/flashback/mixin/playback/MixinEntity.java
+++ src/main/java/com/moulberry/flashback/mixin/playback/MixinEntity.java
@@ -1,12 +1,12 @@
 package com.moulberry.flashback.mixin.playback;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.moulberry.flashback.Flashback;
 import com.moulberry.flashback.playback.ReplayServer;
 import com.moulberry.flashback.state.EditorState;
 import com.moulberry.flashback.state.EditorStateManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.UUID;
@@ -33,6 +32,15 @@ public abstract class MixinEntity {
 
     @Shadow
     public abstract UUID getUUID();
+
+    // Force entities to be able to ride players on servers
+    @WrapOperation(method = "startRiding(Lnet/minecraft/world/entity/Entity;Z)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isClientSide()Z"))
+    public boolean startRiding_isClientSide(Level instance, Operation<Boolean> original) {
+        if (Flashback.isInReplay()) {
+            return true; // Always pretend we're clientside so mounting players is allowed
+        }
+        return original.call(instance);
+    }
 
     @Inject(method = "isInvisibleTo", at = @At("HEAD"), cancellable = true)
     public void isInvisibleTo(Player player, CallbackInfoReturnable<Boolean> cir) {
