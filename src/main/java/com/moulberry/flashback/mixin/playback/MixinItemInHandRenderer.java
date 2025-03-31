f02aed28164e3449f78f0c08eb30d0503604c09a
Moulberry <james.jenour@protonmail.com>
2025-03-31 13:43:28 +0800

    Add view bobbing when spectating player in first person

--- src/main/java/com/moulberry/flashback/mixin/playback/MixinItemInHandRenderer.java
+++ src/main/java/com/moulberry/flashback/mixin/playback/MixinItemInHandRenderer.java
@@ -102,10 +102,10 @@ public abstract class MixinItemInHandRenderer implements ItemInHandRendererExt {
         float h = Mth.lerp(partialTick, clientPlayer.xRotO, clientPlayer.getXRot());
          int handRenderSelection = evaluateWhichHandsToRender(clientPlayer);
         if (clientPlayer instanceof RemotePlayerExt remotePlayerExt) {
-//            float xBob = remotePlayerExt.flashback$getXBob(partialTick);
-//            float yBob = remotePlayerExt.flashback$getYBob(partialTick);
-//            poseStack.mulPose(Axis.XP.rotationDegrees((clientPlayer.getViewXRot(partialTick) - xBob) * 0.1f));
-//            poseStack.mulPose(Axis.YP.rotationDegrees((clientPlayer.getViewYRot(partialTick) - yBob) * 0.1f));
+            float xBob = remotePlayerExt.flashback$getXBob(partialTick);
+            float yBob = remotePlayerExt.flashback$getYBob(partialTick);
+            poseStack.mulPose(Axis.XP.rotationDegrees((clientPlayer.getViewXRot(partialTick) - xBob) * 0.1f));
+            poseStack.mulPose(Axis.YP.rotationDegrees((clientPlayer.getViewYRot(partialTick) - yBob) * 0.1f));
         }
         if ((handRenderSelection & RENDER_MAIN_HAND) != 0) {
             l = interactionHand == InteractionHand.MAIN_HAND ? g : 0.0f;
