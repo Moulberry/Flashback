ece9dcc6b1bb6333d15349e6c1f81959be55ee3e
Moulberry <james.jenour@protonmail.com>
2025-06-13 22:16:55 +0800

    Clean up some imports

--- src/main/java/com/moulberry/flashback/screen/ConfigScreen.java
+++ src/main/java/com/moulberry/flashback/screen/ConfigScreen.java
@@ -1,14 +1,10 @@
 package com.moulberry.flashback.screen;
 
 import com.moulberry.flashback.Flashback;
-import com.moulberry.flashback.configuration.FlashbackConfig;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.options.OptionsSubScreen;
 import net.minecraft.network.chat.Component;
-
-import java.nio.file.Path;
 
 public class ConfigScreen extends OptionsSubScreen {
 
