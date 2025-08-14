b420082eef169608cc2d9aa6e76eef35a409b65d
Moulberry <james.jenour@protonmail.com>
2025-08-14 21:42:54 +0800

    Fix Iris hand rendering in first person

--- src/main/java/com/moulberry/flashback/ext/ItemInHandRendererExt.java
+++ src/main/java/com/moulberry/flashback/ext/ItemInHandRendererExt.java
@@ -3,9 +3,13 @@ package com.moulberry.flashback.ext;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.InteractionHand;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
 
 public interface ItemInHandRendererExt {
 
-    void flashback$renderHandsWithItems(float f, PoseStack poseStack, MultiBufferSource.BufferSource bufferSource, AbstractClientPlayer localPlayer, int i);
+    void flashback$renderHandsWithItems(float f, PoseStack poseStack, MultiBufferSource.BufferSource bufferSource, AbstractClientPlayer localPlayer, int i, @Nullable Set<InteractionHand> renderable);
 
 }
