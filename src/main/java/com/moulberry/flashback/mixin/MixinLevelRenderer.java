81a89fa987a52e3ed4800ab889b9717030714e2e
Moulberry <james.jenour@protonmail.com>
2025-10-17 14:03:33 +0800

    Fix rain particles and sounds not playing

--- src/main/java/com/moulberry/flashback/mixin/MixinLevelRenderer.java
+++ src/main/java/com/moulberry/flashback/mixin/MixinLevelRenderer.java
@@ -13,7 +13,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.textures.GpuTexture;
 import com.mojang.blaze3d.textures.TextureFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexFormat;
 import com.moulberry.flashback.Flashback;
 import com.moulberry.flashback.playback.ReplayServer;
 import com.moulberry.flashback.state.EditorState;
@@ -33,6 +32,7 @@ import net.minecraft.client.renderer.chunk.SectionRenderDispatcher;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.TickRateManager;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import org.joml.Vector4f;
@@ -64,13 +64,20 @@ public abstract class MixinLevelRenderer {
 
     @Shadow @Final private LevelTargetBundle targets;
 
-    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tick", at = @At("HEAD"))
     public void tick(CallbackInfo ci) {
         ReplayServer replayServer = Flashback.getReplayServer();
         if (replayServer != null) {
             this.ticks = replayServer.getReplayTick();
-            ci.cancel();
         }
+    }
+
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickRateManager;runsNormally()Z"))
+    public boolean tick_runsNormally(TickRateManager instance, Operation<Boolean> original) {
+        if (Flashback.isInReplay()) {
+            return false;
+        }
+        return original.call(instance);
     }
 
     @Inject(method = "renderLevel", at = @At("HEAD"))
