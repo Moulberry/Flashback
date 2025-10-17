0e40c8ad1344abd93f05591b0a1cc451d3c83be6
Moulberry <james.jenour@protonmail.com>
2025-10-17 12:00:20 +0800

    Send custom payloads collected during snapshot on player join

--- src/main/java/com/moulberry/flashback/playback/ReplayGamePacketHandler.java
+++ src/main/java/com/moulberry/flashback/playback/ReplayGamePacketHandler.java
@@ -1630,6 +1630,10 @@ public class ReplayGamePacketHandler implements ClientGamePacketListener {
             return;
         }
 
+        if (this.replayServer.isProcessingSnapshot) {
+            this.replayServer.customPacketsInSnapshot.add(clientboundCustomPayloadPacket);
+        }
+
         forward(clientboundCustomPayloadPacket);
     }
 
