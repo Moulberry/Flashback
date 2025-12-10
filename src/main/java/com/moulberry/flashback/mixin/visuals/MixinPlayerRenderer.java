3cae6f30ac915b69a51d41fa22737fe0a7ff78a1
Moulberry <james.jenour@protonmail.com>
2025-12-10 19:39:42 +0800

    Add selected entity toggle for model parts (e.g. cape)

--- src/main/java/com/moulberry/flashback/mixin/visuals/MixinPlayerRenderer.java
+++ src/main/java/com/moulberry/flashback/mixin/visuals/MixinPlayerRenderer.java
@@ -3,6 +3,8 @@ package com.moulberry.flashback.mixin.visuals;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.moulberry.flashback.state.EditorState;
 import com.moulberry.flashback.state.EditorStateManager;
 import net.minecraft.client.player.AbstractClientPlayer;
@@ -10,8 +12,12 @@ import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.scores.DisplaySlot;
 import net.minecraft.world.scores.Objective;
 import net.minecraft.world.scores.Scoreboard;
+import net.minecraft.client.renderer.entity.state.PlayerRenderState;
+import net.minecraft.world.entity.player.PlayerModelPart;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.EnumSet;
 
 @Mixin(PlayerRenderer.class)
 public class MixinPlayerRenderer {
@@ -23,6 +29,27 @@ public class MixinPlayerRenderer {
             return null;
         }
         return original.call(instance, displaySlot);
+    }
+
+
+    @Inject(method = "extractRenderState(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/client/renderer/entity/state/PlayerRenderState;F)V", at = @At("HEAD"))
+    public void extractRenderStateHead(AbstractClientPlayer player, PlayerRenderState playerRenderState, float f, CallbackInfo ci,
+        @Share(value = "hiddenModelParts", namespace = "flashback") LocalRef<EnumSet<PlayerModelPart>> hiddenPartsRef) {
+        EnumSet<PlayerModelPart> hiddenParts = null;
+
+        EditorState editorState = EditorStateManager.getCurrent();
+        if (editorState != null) {
+            hiddenParts = editorState.hiddenModelParts.get(player.getUUID());
+        }
+
+        hiddenPartsRef.set(hiddenParts);
+    }
+
+    @WrapOperation(method = "extractRenderState(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/client/renderer/entity/state/PlayerRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/AbstractClientPlayer;isModelPartShown(Lnet/minecraft/world/entity/player/PlayerModelPart;)Z"))
+    public boolean extractRenderState_isModelPartShown(AbstractClientPlayer instance, PlayerModelPart playerModelPart, Operation<Boolean> original,
+        @Share(value = "hiddenModelParts", namespace = "flashback") LocalRef<EnumSet<PlayerModelPart>> hiddenPartsRef) {
+        var hiddenParts = hiddenPartsRef.get();
+        return hiddenParts == null ? original.call(instance, playerModelPart) : !hiddenParts.contains(playerModelPart);
     }
 
 }
