0bd55fb9ae68437586a1eeea6f579505a3b11e82
Moulberry <james.jenour@protonmail.com>
2025-12-22 19:03:41 +0800

    Backport to 1.21.5

--- src/main/java/com/moulberry/flashback/mixin/replay_server/MixinPlayerList.java
+++ src/main/java/com/moulberry/flashback/mixin/replay_server/MixinPlayerList.java
@@ -3,20 +3,26 @@ package com.moulberry.flashback.mixin.replay_server;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.moulberry.flashback.playback.FakePlayer;
 import com.moulberry.flashback.playback.ReplayServer;
 import net.fabricmc.fabric.impl.event.interaction.FakePlayerNetworkHandler;
 import net.minecraft.network.Connection;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.level.Level;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @Mixin(PlayerList.class)
 public class MixinPlayerList {
