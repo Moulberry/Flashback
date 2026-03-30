1db8893c301ab5480c0dca21f7fa956b764218fa
Moulberry <james.jenour@protonmail.com>
2026-03-30 20:52:20 +0800

    Prevent invisibility/glowing data from being overriden by potion effects

--- src/main/java/com/moulberry/flashback/mixin/playback/MixinLivingEntity.java
+++ src/main/java/com/moulberry/flashback/mixin/playback/MixinLivingEntity.java
@@ -1,42 +1,36 @@
 package com.moulberry.flashback.mixin.playback;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
+import com.moulberry.flashback.Flashback;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity extends Entity {
+public class MixinLivingEntity {
 
-    @Shadow
-    public abstract void lerpTo(double d, double e, double f, float g, float h, int i);
+    // Prevent invisible/glowing state from being updated based on potion effects inside a replay
 
-    @Shadow
-    public abstract void lerpHeadTo(float f, int i);
-
-    public MixinLivingEntity(EntityType<?> entityType, Level level) {
-        super(entityType, level);
+    @Inject(method = "updateDirtyEffects", at = @At("HEAD"), cancellable = true)
+    public void updateDirtyEffects(CallbackInfo ci) {
+        if (Flashback.isInReplay()) {
+            ci.cancel();
+        }
     }
 
-    // todo: maybe reintroduce this lerpTo code?
-//    @Inject(method = "lerpTo", at = @At("HEAD"), cancellable = true)
-//    public void lerpTo(double d, double e, double f, float g, float h, int i, CallbackInfo ci) {
-//        if (FlashbackClient.isInReplay() && i > 1 && FlashbackClient.getReplayServer().getLocalPlayerId() == this.getId() && Minecraft.getInstance().getCameraEntity() == this) {
-//            this.lerpTo(d, e, f, g, h, 1);
-//            ci.cancel();
-//        }
-//    }
-//
-//    @Inject(method = "lerpHeadTo", at = @At("HEAD"), cancellable = true)
-//    public void lerpHeadTo(float f, int i, CallbackInfo ci) {
-//        if (FlashbackClient.isInReplay() && i > 1 && FlashbackClient.getReplayServer().getLocalPlayerId() == this.getId() && Minecraft.getInstance().getCameraEntity() == this) {
-//            this.lerpHeadTo(f, 1);
-//            ci.cancel();
-//        }
-//    }
+    @Inject(method = "updateInvisibilityStatus", at = @At("HEAD"), cancellable = true)
+    public void updateInvisibilityStatus(CallbackInfo ci) {
+        if (Flashback.isInReplay()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "updateGlowingStatus", at = @At("HEAD"), cancellable = true)
+    public void updateGlowingStatus(CallbackInfo ci) {
+        if (Flashback.isInReplay()) {
+            ci.cancel();
+        }
+    }
 
 }
