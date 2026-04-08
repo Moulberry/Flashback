b1e48ef83f0dc0b177d982c00034d5bce87affec
Fiereu <contact@fiereu.de>
2026-04-08 06:59:07 +0200

    fix: compatibility with Iris 1.10.9

--- src/main/java/com/moulberry/flashback/mixin/compat/iris/MixinIrisHandRenderer.java
+++ src/main/java/com/moulberry/flashback/mixin/compat/iris/MixinIrisHandRenderer.java
@@ -14,8 +14,8 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.SubmitNodeStorage;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -34,16 +34,16 @@ import java.util.Objects;
 public abstract class MixinIrisHandRenderer {
 
     @Shadow
-    public abstract boolean isHandTranslucent(InteractionHand hand);
+    public abstract boolean isHandTranslucent(ItemStack itemStack);
 
     @Unique
     private static final InteractionHand[] HANDS = InteractionHand.values();
 
     @Inject(method = "isHandTranslucent", at = @At("HEAD"), cancellable = true, require = 0, remap = false)
-    public void isHandTransluent(InteractionHand hand, CallbackInfoReturnable<Boolean> cir) {
+    public void isHandTranslucent(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
         AbstractClientPlayer spectatingPlayer = Flashback.getSpectatingPlayer();
         if (spectatingPlayer != null) {
-            var item = spectatingPlayer.getItemBySlot(hand == InteractionHand.OFF_HAND ? EquipmentSlot.OFFHAND : EquipmentSlot.MAINHAND).getItem();
+            var item = itemStack.getItem();
 
             if (item instanceof BlockItem blockItem) {
                 cir.setReturnValue(Minecraft.getInstance().getModelManager().getBlockStateModelSet().get(blockItem.getBlock().defaultBlockState()).hasMaterialFlag(1));
@@ -68,7 +68,8 @@ public abstract class MixinIrisHandRenderer {
         if (spectatingPlayer != null) {
             EnumSet<InteractionHand> renderableArms = EnumSet.noneOf(InteractionHand.class);
             for (InteractionHand hand : HANDS) {
-                if (!this.isHandTranslucent(hand)) renderableArms.add(hand);
+                ItemStack itemStack = flashback$getItemStackOfHand(spectatingPlayer, hand);
+                if (!this.isHandTranslucent(itemStack)) renderableArms.add(hand);
             }
             float frozenPartialTick = Minecraft.getInstance().level.tickRateManager().isEntityFrozen(spectatingPlayer) ? 1.0f : tickDelta;
             ((ItemInHandRendererExt)instance).flashback$renderHandsWithItems(frozenPartialTick, poseStack, submitNodeStorage, spectatingPlayer, i, renderableArms);
@@ -83,13 +84,19 @@ public abstract class MixinIrisHandRenderer {
         if (spectatingPlayer != null) {
             EnumSet<InteractionHand> renderableArms = EnumSet.noneOf(InteractionHand.class);
             for (InteractionHand hand : HANDS) {
-                if (this.isHandTranslucent(hand)) renderableArms.add(hand);
+                ItemStack itemStack = flashback$getItemStackOfHand(spectatingPlayer, hand);
+                if (this.isHandTranslucent(itemStack)) renderableArms.add(hand);
             }
             float frozenPartialTick = Minecraft.getInstance().level.tickRateManager().isEntityFrozen(spectatingPlayer) ? 1.0f : tickDelta;
             ((ItemInHandRendererExt)instance).flashback$renderHandsWithItems(frozenPartialTick, poseStack, submitNodeStorage, spectatingPlayer, i, renderableArms);
         } else {
             original.call(instance, handRenderer, tickDelta, poseStack, submitNodeStorage, localPlayer, i);
         }
+    }
+
+    @Unique
+    private ItemStack flashback$getItemStackOfHand(AbstractClientPlayer player, InteractionHand hand) {
+      return player.getItemInHand(hand).getItem().getDefaultInstance();
     }
 
 }
