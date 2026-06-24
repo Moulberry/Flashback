63e130f92e94c6463d1297ada0931351368bced2
Moulberry <james.jenour@protonmail.com>
2026-06-24 23:43:41 +0800

    Make lenient registry loader return correct value

--- src/main/java/com/moulberry/flashback/mixin/playback/lenient_registry/MixinRegistry.java
+++ src/main/java/com/moulberry/flashback/mixin/playback/lenient_registry/MixinRegistry.java
@@ -3,10 +3,9 @@ package com.moulberry.flashback.mixin.playback.lenient_registry;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.serialization.DataResult;
 import com.moulberry.flashback.Flashback;
-import com.moulberry.flashback.playback.ReplayConfigurationPacketHandler;
 import net.minecraft.core.DefaultedRegistry;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.BuiltInRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,15 +14,15 @@ import org.spongepowered.asm.mixin.injection.At;
 public interface MixinRegistry {
 
     @ModifyReturnValue(method = "lambda$referenceHolderWithLifecycle$0", at = @At("RETURN"))
-    default DataResult<?> referenceHolderWithLifecycle_get(DataResult<?> original) {
+    default DataResult<Holder.Reference<?>> referenceHolderWithLifecycle_get(DataResult<Holder.Reference<?>> original) {
         if (original.isSuccess()) {
             return original;
         }
         if (Flashback.isInReplay()) {
             if (this == BuiltInRegistries.PARTICLE_TYPE) {
-                return DataResult.success(ParticleTypes.ASH);
+                return DataResult.success(BuiltInRegistries.PARTICLE_TYPE.get(0).get());
             } else if (this instanceof DefaultedRegistry<?> defaultedRegistry) {
-                return DataResult.success(defaultedRegistry.getValue(defaultedRegistry.getDefaultKey()));
+                return DataResult.success(defaultedRegistry.get(defaultedRegistry.getDefaultKey()).get());
             }
         }
         return original;
