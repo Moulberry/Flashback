5adc0f9f7d8cf27c34e842a335a9d73d8349a5fc
Moulberry <james.jenour@protonmail.com>
2026-07-28 19:48:25 +0800

    1.21.11 and below: yarn rename fix packetlistener -> networkhandler

--- src/main/java/com/moulberry/flashback/playback/FlashbackFakePlayerPacketListener.java
+++ src/main/java/com/moulberry/flashback/playback/FlashbackFakePlayerPacketListener.java
@@ -1,13 +1,13 @@
 package com.moulberry.flashback.playback;
 
-import net.fabricmc.fabric.impl.networking.UntrackedPacketListener;
+import net.fabricmc.fabric.impl.networking.UntrackedNetworkHandler;
 import net.minecraft.network.Connection;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 
-public class FlashbackFakePlayerPacketListener extends ServerGamePacketListenerImpl implements UntrackedPacketListener {
+public class FlashbackFakePlayerPacketListener extends ServerGamePacketListenerImpl implements UntrackedNetworkHandler {
 
     public FlashbackFakePlayerPacketListener(MinecraftServer server, Connection connection, ServerPlayer player, CommonListenerCookie cookie) {
         super(server, connection, player, cookie);
