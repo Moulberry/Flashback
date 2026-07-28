25641c21d86bb90a03dd172a880d192f9088d489
Moulberry <james.jenour@protonmail.com>
2026-07-28 14:55:20 +0800

    Add 60 dummy frames at start of export

--- src/main/java/com/moulberry/flashback/exporting/ExportJob.java
+++ src/main/java/com/moulberry/flashback/exporting/ExportJob.java
@@ -368,6 +368,9 @@ public class ExportJob {
 
             long pauseScreenStart = System.currentTimeMillis();
             int additionalDummyFrames = this.extraDummyFrames;
+            if (tickIndex == 0) {
+                additionalDummyFrames += 60;
+            }
             while (mc.gui.overlay() != null || mc.gui.screen() != null || additionalDummyFrames > 0) {
                 boolean overlayOrScreen = mc.gui.overlay() != null || mc.gui.screen() != null;
                 if (overlayOrScreen) {
@@ -381,13 +384,12 @@ public class ExportJob {
                 render(renderTarget, mc.deltaTracker);
 
                 if (overlayOrScreen) {
-                    this.shouldChangeFramebufferSize = false;
-                    if (!mc.getWindow().isMinimized()) {
-                        renderTarget.blitToScreen();
-                    }
-                    RenderSystem.flipFrame(null);
-                    this.shouldChangeFramebufferSize = true;
+                    finishFrame(renderTarget, new ArrayList<>(List.of("Waiting for overlay to disappear")), true, false);
+                } else if (tickIndex == 0) {
+                    finishFrame(renderTarget, new ArrayList<>(List.of("Warming up... " + additionalDummyFrames + "/60")), true, false);
+                }
 
+                if (overlayOrScreen) {
                     LockSupport.parkNanos("waiting for pause overlay to disappear", 50_000_000L);
 
                     // Force remove screens/overlays after 5s/15s respectively
