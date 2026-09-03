abcdc6fc982ab1b2cbd2085243f5e0da789f6143
Moulberry <james.jenour@protonmail.com>
2026-09-03 15:57:11 +0800

    Add minimum tickrate and add sanity checks to export tick calculation

--- src/main/java/com/moulberry/flashback/keyframe/change/KeyframeChangeTickrate.java
+++ src/main/java/com/moulberry/flashback/keyframe/change/KeyframeChangeTickrate.java
@@ -4,6 +4,8 @@ import com.moulberry.flashback.Interpolation;
 import com.moulberry.flashback.keyframe.handler.KeyframeHandler;
 
 public record KeyframeChangeTickrate(float tickrate) implements KeyframeChange {
+    public static final float MIN_TICKRATE = 0.001f;
+
     @Override
     public void apply(KeyframeHandler keyframeHandler) {
         keyframeHandler.applyTickrate(this.tickrate);
@@ -13,7 +15,7 @@ public record KeyframeChangeTickrate(float tickrate) implements KeyframeChange {
     public KeyframeChange interpolate(KeyframeChange to, double amount) {
         KeyframeChangeTickrate other = (KeyframeChangeTickrate) to;
         return new KeyframeChangeTickrate(
-            (float) Interpolation.linear(this.tickrate, other.tickrate, amount)
+            Math.max(MIN_TICKRATE, (float) Interpolation.linear(this.tickrate, other.tickrate, amount))
         );
     }
 
