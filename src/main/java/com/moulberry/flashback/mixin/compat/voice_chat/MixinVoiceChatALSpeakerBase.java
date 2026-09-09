315700fcd876b7b391f04cf286cc315eae09f0c6
Max Henkel <max@maxhenkel.de>
2026-09-09 08:27:05 +0200

    Fix Mixin errors with Simple Voice Chat 2.6.23+

--- src/main/java/com/moulberry/flashback/mixin/compat/voice_chat/MixinVoiceChatALSpeakerBase.java
+++ src/main/java/com/moulberry/flashback/mixin/compat/voice_chat/MixinVoiceChatALSpeakerBase.java
@@ -5,9 +5,10 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.moulberry.flashback.state.EditorState;
 import com.moulberry.flashback.state.EditorStateManager;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
+import de.maxhenkel.voicechat.voice.client.camera.CameraState;
 import de.maxhenkel.voicechat.voice.client.speaker.ALSpeakerBase;
 import net.minecraft.client.Camera;
-import net.minecraft.client.renderer.GameRenderer;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,16 +18,25 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(ALSpeakerBase.class)
 public class MixinVoiceChatALSpeakerBase {
 
-    @WrapOperation(method = "setPositionSync", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;mainCamera()Lnet/minecraft/client/Camera;"))
-    public Camera setPositionSync_getMainCamera(GameRenderer instance, Operation<Camera> original) {
+    @WrapOperation(method = "setPositionSync", at = @At(value = "INVOKE", target = "Lde/maxhenkel/voicechat/voice/client/ClientManager;getCameraState()Lde/maxhenkel/voicechat/voice/client/camera/CameraState;"))
+    public CameraState setPositionSync_getCameraState(Operation<CameraState> original) {
         EditorState editorState = EditorStateManager.getCurrent();
         if (editorState != null) {
             Camera audioCamera = editorState.getAudioCamera();
             if (audioCamera != null) {
-                return audioCamera;
+                return new CameraState(
+                        audioCamera.position(),
+                        new Vector3f(audioCamera.forwardVector()),
+                        new Vector3f(audioCamera.upVector()),
+                        audioCamera.yRot(),
+                        null,
+                        audioCamera.position(),
+                        null,
+                        false
+                );
             }
         }
-        return original.call(instance);
+        return original.call();
     }
 
 }
