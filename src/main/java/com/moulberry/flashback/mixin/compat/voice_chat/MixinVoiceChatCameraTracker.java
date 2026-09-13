10165e1574fce0d72d4b3902da74c1ba8a23c7f6
Moulberry <james.jenour@protonmail.com>
2026-09-14 01:08:03 +0800

    Fix compatibility with svc >=2.6.23

--- src/main/java/com/moulberry/flashback/mixin/compat/voice_chat/MixinVoiceChatCameraTracker.java
+++ src/main/java/com/moulberry/flashback/mixin/compat/voice_chat/MixinVoiceChatCameraTracker.java
@@ -5,7 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.moulberry.flashback.state.EditorState;
 import com.moulberry.flashback.state.EditorStateManager;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
-import de.maxhenkel.voicechat.voice.client.speaker.ALSpeakerBase;
+import de.maxhenkel.voicechat.voice.client.camera.CameraTracker;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @IfModLoaded("voicechat")
 @Pseudo
-@Mixin(ALSpeakerBase.class)
-public class MixinVoiceChatALSpeakerBase {
+@Mixin(CameraTracker.class)
+public class MixinVoiceChatCameraTracker {
 
-    @WrapOperation(method = "setPositionSync", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;getMainCamera()Lnet/minecraft/client/Camera;"))
+    @WrapOperation(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;getMainCamera()Lnet/minecraft/client/Camera;"))
     public Camera setPositionSync_getMainCamera(GameRenderer instance, Operation<Camera> original) {
         EditorState editorState = EditorStateManager.getCurrent();
         if (editorState != null) {
