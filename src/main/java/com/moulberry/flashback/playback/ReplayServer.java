7806e3e586e9a0bcfb85e04e0fae8b5f462dc68b
Moulberry <james.jenour@protonmail.com>
2026-09-14 01:30:59 +0800

    Fix player info state being wrong after rewind

--- src/main/java/com/moulberry/flashback/playback/ReplayServer.java
+++ src/main/java/com/moulberry/flashback/playback/ReplayServer.java
@@ -1539,6 +1539,7 @@ public class ReplayServer extends IntegratedServer {
             }
         }
         this.bossEvents.clear();
+        this.gamePacketHandler.clearDataForPlayingSnapshot();
     }
 
     public void blockChangeOccurred(BlockPos blockPos, BlockState result) {
