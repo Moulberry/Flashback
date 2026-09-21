e9385b14b7eb44e9deba682bfc3b8d8541b66700
Moulberry <james.jenour@protonmail.com>
2026-09-21 13:52:06 +0800

    Update voice chat compat to use internal 2.6.24 api

--- src/main/java/com/moulberry/flashback/compat/simple_voice_chat/SimpleVoiceChatPlugin.java
+++ src/main/java/com/moulberry/flashback/compat/simple_voice_chat/SimpleVoiceChatPlugin.java
@@ -1,6 +1,9 @@
 package com.moulberry.flashback.compat.simple_voice_chat;
 
+import com.moulberry.flashback.Flashback;
 import com.moulberry.flashback.action.ActionRegistry;
+import com.moulberry.flashback.state.EditorState;
+import com.moulberry.flashback.state.EditorStateManager;
 import de.maxhenkel.voicechat.api.VoicechatApi;
 import de.maxhenkel.voicechat.api.VoicechatClientApi;
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
@@ -8,6 +11,15 @@ import de.maxhenkel.voicechat.api.events.ClientReceiveSoundEvent;
 import de.maxhenkel.voicechat.api.events.ClientSoundEvent;
 import de.maxhenkel.voicechat.api.events.ClientVoicechatInitializationEvent;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
+import de.maxhenkel.voicechat.api.events.NameTagIconRenderEvent;
+import de.maxhenkel.voicechat.api.internal.events.AlcContextEvent;
+import de.maxhenkel.voicechat.api.internal.events.ForceShowIconsEvent;
+import de.maxhenkel.voicechat.api.internal.events.HudIconRenderEvent;
+import de.maxhenkel.voicechat.api.internal.events.NameTagIconRenderEventExtension;
+import de.maxhenkel.voicechat.api.internal.events.StartMicEvent;
+import de.maxhenkel.voicechat.api.internal.events.UpdateCameraPositionEvent;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
 
 public class SimpleVoiceChatPlugin implements VoicechatPlugin {
 
@@ -30,9 +42,57 @@ public class SimpleVoiceChatPlugin implements VoicechatPlugin {
         registration.registerEvent(ClientReceiveSoundEvent.LocationalSound.class, SimpleVoiceChatRecorder::onReceiveLocationalSound);
         registration.registerEvent(ClientReceiveSoundEvent.StaticSound.class, SimpleVoiceChatRecorder::onReceiveStaticSound);
         registration.registerEvent(ClientSoundEvent.class, SimpleVoiceChatRecorder::onSendSound);
+        registration.registerEvent(UpdateCameraPositionEvent.class, this::updateCameraPosition);
+        registration.registerEvent(StartMicEvent.class, this::onStartMic);
+        registration.registerEvent(NameTagIconRenderEvent.class, this::onRenderNameTagIcon);
+        registration.registerEvent(HudIconRenderEvent.class, this::onRenderHudIcons);
+        registration.registerEvent(AlcContextEvent.class, this::onAlcContext);
+        registration.registerEvent(ForceShowIconsEvent.class, this::onForceShowIcons);
     }
 
     private void onClientInitialized(ClientVoicechatInitializationEvent event) {
         CLIENT_API = event.getVoicechat();
     }
+
+    private void updateCameraPosition(UpdateCameraPositionEvent event) {
+        EditorState editorState = EditorStateManager.getCurrent();
+        if (editorState != null) {
+            Camera audioCamera = editorState.getAudioCamera();
+            if (audioCamera != null) {
+                event.setCameraPosition(audioCamera.getPosition(), audioCamera.getLookVector(), audioCamera.getUpVector());
+            }
+        }
+    }
+
+    private void onStartMic(StartMicEvent event) {
+        if (Flashback.isInReplay()) {
+            event.cancel();
+        }
+    }
+
+    private void onRenderNameTagIcon(NameTagIconRenderEvent event) {
+        if (Flashback.isInReplay() && ((NameTagIconRenderEventExtension) event).isDisconnected()) {
+            event.cancel();
+        }
+    }
+
+    private void onRenderHudIcons(HudIconRenderEvent event) {
+        if (Flashback.isInReplay()) {
+            event.cancel();
+        }
+    }
+
+    private void onAlcContext(AlcContextEvent event) {
+        if (Flashback.isExporting() && Flashback.EXPORT_JOB.getSettings().recordAudio()) {
+            long context = Minecraft.getInstance().getSoundManager().soundEngine.library.context;
+            event.setContext(context);
+        }
+    }
+
+    private void onForceShowIcons(ForceShowIconsEvent event) {
+        if (Flashback.isInReplay()) {
+            event.forceShow();
+        }
+    }
+
 }
