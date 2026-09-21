41e3f660163dce760afc85b98d5a5c7062e03ceb
Moulberry <james.jenour@protonmail.com>
2026-09-21 14:30:50 +0800

    Store selected encoder as String

--- src/main/java/com/moulberry/flashback/configuration/ForceDefaultExportSettings.java
+++ src/main/java/com/moulberry/flashback/configuration/ForceDefaultExportSettings.java
@@ -16,7 +16,7 @@ public class ForceDefaultExportSettings {
 
     public VideoContainer container = null;
     public VideoCodec videoCodec = null;
-    public int[] selectedVideoEncoder = null;
+    public String selectedVideoEncoder = null;
     public Boolean useMaximumBitrate = null;
 
     public Boolean recordAudio = null;
@@ -49,7 +49,7 @@ public class ForceDefaultExportSettings {
             config.videoCodec = this.videoCodec;
         }
         if (this.selectedVideoEncoder != null) {
-            config.selectedVideoEncoder =  Arrays.copyOf(this.selectedVideoEncoder, 1);
+            config.selectedVideoEncoder = this.selectedVideoEncoder;
         }
         if (this.useMaximumBitrate != null) {
             config.useMaximumBitrate = this.useMaximumBitrate;
