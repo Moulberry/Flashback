14fb78dc0bcb2bbc428abf507c46e066a22ef2c9
Moulberry <james.jenour@protonmail.com>
2026-09-21 14:30:50 +0800

    Store selected encoder as String

--- src/main/java/com/moulberry/flashback/configuration/FlashbackConfigV0.java
+++ src/main/java/com/moulberry/flashback/configuration/FlashbackConfigV0.java
@@ -55,7 +55,6 @@ public class FlashbackConfigV0 {
 
     public VideoContainer container = null;
     public VideoCodec videoCodec = null;
-    public int[] selectedVideoEncoder = new int[]{0};
     public boolean useMaximumBitrate = false;
 
     public boolean recordAudio = false;
@@ -126,7 +125,6 @@ public class FlashbackConfigV0 {
         config.internalExport.noGui = this.noGui;
         config.internalExport.container = this.container;
         config.internalExport.videoCodec = this.videoCodec;
-        config.internalExport.selectedVideoEncoder = this.selectedVideoEncoder;
         config.internalExport.useMaximumBitrate = this.useMaximumBitrate;
         config.internalExport.recordAudio = this.recordAudio;
         config.internalExport.transparentBackground = this.transparentBackground;
